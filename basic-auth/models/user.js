a6c129d4b877c32a5c545f9f24aa830cecd2ff74
jose luis <joseluisblascoortiz@hotmail.com>
2020-10-27 17:10:27 +0100

    1 commit

--- basic-auth/models/user.js
+++ basic-auth/models/user.js
@@ -4,8 +4,26 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    email: String,
-    password: String,
+    fullname: String,
+    birthdate: Date,
+    genre:String,
+    mail:String,
+    photo:String,
+    question1:String,
+    question2:String,
+    question3:String,
+    question4:String,
+    question5:String,
+    question6:String,
+    question7:String,
+    question8:String,
+    question9:String,
+    question10:String,
+    question11:String,
+    question12:String,
+    question13:String,
+    question14:String,
+    question15:String,
   },
   {
     timestamps: true,
