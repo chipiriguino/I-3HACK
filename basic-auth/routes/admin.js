d3c47a8d7bd28fb118f61fa3bfeacdfb6de53710
Marta Camacho <martacamachovargas@gmail.com>
2020-10-28 16:34:04 +0100

    readme updated

---
+++ basic-auth/routes/admin.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+router.get('/dashboard', (req, res, next) => {
+
+    // renderizamos la vista 'dashboard' dentro de nuestra carpeta "laundry" de vistas});
+
+    }
+    
+module.exports = router;
